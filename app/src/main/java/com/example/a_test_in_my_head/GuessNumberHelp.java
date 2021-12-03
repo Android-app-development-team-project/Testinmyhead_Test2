9ff32d923a5490575690619aaf53970fc5ecc8cf
ans9971 <93367718+ans9971@users.noreply.github.com>
2021-12-03 16:09:02 +0900

    '12/03'

---
+++ app/src/main/java/com/example/a_test_in_my_head/GuessNumberHelp.java
@@ -0,0 +1,43 @@
+package com.example.a_test_in_my_head;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class GuessNumberHelp extends AppCompatActivity {
+    long backKeyPressedTime = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess_number_help);
+        Button btn = (Button) findViewById(R.id.backmainbtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        if(System.currentTimeMillis()>backKeyPressedTime+2000){
+            backKeyPressedTime=System.currentTimeMillis();
+            Toast.makeText(this,"뒤로가기 버튼을 한번 더 누르시면 return home!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(System.currentTimeMillis()<=backKeyPressedTime+2000){
+            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        }
+
+    }
+}
